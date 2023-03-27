5a11b1b55a401e61b291165568d0cfe11d619696
Kartik <kartik.gowda55@gmail.com>
2023-03-27 18:56:26 +1100

    Cleaning List.ejs page

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   let today = new Date();
-  // let day = today.toLocaleDateString('en-US', { weekday: 'long' }); // ! Using New method
 
   const options = {
     weekday: 'long',
@@ -16,23 +15,9 @@ app.get('/', (req, res) => {
     month: 'long',
   };
 
-  let day = today.toLocaleDateString('en-US', options); // ! Using New method
-  res.render('list', { todayDayType: day });
+  let day = today.toLocaleString('en-US', options);
+  res.render('list', { todayDay: day });
 });
-
-// app.get('/', (req, res) => {
-//   let today = new Date();
-
-//   if (today.getDay() === 6 || today.getDay() === 0) {
-//     // console.log('Today is a weekend!');
-//     res.write('<h1>Today is a weekend!</h1>');
-//   } else {
-//     // console.log('Today is a weekday!');
-//     res.write('<h1>Today is a weekday!</h1>');
-//     res.write('<h2>BooHoo! This gentleman has to work today!!</h2>');
-//     res.send();
-//   }
-// });
 
 app.listen(3000, () => {
   console.log('Todoist App listening on port 3000!');
