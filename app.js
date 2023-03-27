315bf9d547c1e8edd43e65563565f52516c8970e
Kartik <kartik.gowda55@gmail.com>
2023-03-27 18:55:32 +1100

    Changing Display to Day,Date-Month Format for List

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   let today = new Date();
-  // let day = today.toLocaleDateString('en-US', { weekday: 'long' });// ! Using New method
+  // let day = today.toLocaleDateString('en-US', { weekday: 'long' }); // ! Using New method
 
   const options = {
     weekday: 'long',
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
     month: 'long',
   };
 
-  let day = today.getDay().toLocaleString('en-US', options); // ! Using New method
+  let day = today.toLocaleDateString('en-US', options); // ! Using New method
   res.render('list', { todayDayType: day });
 });
 
