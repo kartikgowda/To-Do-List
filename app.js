85b0e1e6c19962c1285fe71cada458ba4cbed147
Kartik <kartik.gowda55@gmail.com>
2023-03-28 13:08:00 +1100

    Added Description for tasks and cleaned

--- app.js
+++ app.js
@@ -7,15 +7,20 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+  // Create a new date object
   let today = new Date();
 
+  // Create an options object with the day, month, and year
   const options = {
     weekday: 'long',
     day: 'numeric',
     month: 'long',
   };
 
+  // Use the options object and the toLocaleString method to create a string for today's date
   let day = today.toLocaleString('en-US', options);
+
+  // Render the list.ejs file and pass in the date string
   res.render('list', { todayDay: day });
 });
 
