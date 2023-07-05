22973a2338ce07f6a1df43fc30542ad3c6d94510
Kartik <kartik.gowda55@gmail.com>
2023-07-05 12:55:11 +1000

    Port Changes for Heroku

--- app.js
+++ app.js
@@ -145,6 +145,12 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.listen(3000, () => {
-  console.log('Todoist App listening on port 3000!');
+let port = process.env.PORT;
+
+if (port == null || port == '') {
+  port = 3000;
+}
+app.listen(port, () => {
+  console.log('Server is running on port 3000');
+  // console.log('Todoist App listening on port 3000!');
 });
